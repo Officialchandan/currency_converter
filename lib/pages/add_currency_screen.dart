0988ece0c99bb9f5c7b422b922dfd0d1cb4fb999
tekzee-d17 <chandan.vishwakarma@tekzee.com>
2021-10-25 16:18:21 +0530

    commit 25/10/2021 --

--- lib/pages/add_currency_screen.dart
+++ lib/pages/add_currency_screen.dart
@@ -8,6 +8,8 @@ import 'package:currency_converter/database/currencydata.dart';
 import 'package:flutter/material.dart';
 
 class AddCurrency extends StatefulWidget {
+  const AddCurrency({Key? key}) : super(key: key);
+
   @override
   _AddCurrencyState createState() => _AddCurrencyState();
 }
