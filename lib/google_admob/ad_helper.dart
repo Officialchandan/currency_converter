6ff56872d95a7ebe7cf35f146540020526c254d0
tekzee-d17 <chandan.vishwakarma@tekzee.com>
2021-12-14 11:00:53 +0530

    commit - adMob  by chandan ->

---
+++ lib/google_admob/ad_helper.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter_admob_app_open/ad_request_app_open.dart';
+import 'package:flutter_admob_app_open/flutter_admob_app_open.dart';
+
+class AdHelper {
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3096560792937908/8528078048';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static getOpenAdd() async {
+    const appAppOpenAdUnitId = "ca-app-pub-3096560792937908/2624180523";
+    AdRequestAppOpen targetingInfo = const AdRequestAppOpen(
+        keywords: <String>['flutterio', 'beautiful apps'],
+        contentUrl: 'https://flutter.io',
+        nonPersonalizedAds: true,
+        testDevices: ['22c3712a-2a0a-469b-98ca-b6ac0368dd3d']);
+    print("hello Addhelper");
+
+    await FlutterAdmobAppOpen.instance.initialize(
+      appAppOpenAdUnitId: appAppOpenAdUnitId,
+      targetingInfo: targetingInfo,
+      appId: "ca-app-pub-3096560792937908~8277815192",
+    );
+  }
+}
