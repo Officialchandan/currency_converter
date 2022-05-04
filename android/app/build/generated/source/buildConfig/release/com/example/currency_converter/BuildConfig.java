7f6c25be3a5cbea6c70015843d8ebba7941f7676
Pankaj Arya <pankaj.arya@tekzee.com>
2022-05-04 10:49:41 +0530

    app widget

---
+++ android/app/build/generated/source/buildConfig/release/com/example/currency_converter/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.currency_converter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.currencywiki.currencyconverter";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 25;
+  public static final String VERSION_NAME = "3.5";
+}
