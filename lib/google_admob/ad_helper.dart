f46c3d77cad5e2999a349c6b23851df99cde3da4
tekzee-d17 <chandan.vishwakarma@tekzee.com>
2022-07-29 10:58:26 +0530

    29-07-2022 change ad id

--- lib/google_admob/ad_helper.dart
+++ lib/google_admob/ad_helper.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3096560792937908/8528078048';
+      return 'ca-app-pub-3096560792937908~8277815192';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
